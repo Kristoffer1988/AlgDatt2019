03e8a9736b70d0bb655f3dd491349b686497db0e
Kristoffer <kgl_1988@hotmail.no>
2019-08-27 09:35:03 +0200

    Test

---
+++ src/Forelsning270819Test.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Forelsning270819Test {
+
+    @org.junit.jupiter.api.Test
+    void maximum() {
+        int[] values = {5,7,9,2,1,11,13,8};
+
+        assertEquals(13)
+    }
+}
